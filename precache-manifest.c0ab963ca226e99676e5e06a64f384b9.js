dfff4915f6dbc95807abac4452748e3d1cd6d350
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-01-19 19:24:29 -0300

    Updates

---
+++ precache-manifest.c0ab963ca226e99676e5e06a64f384b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4291736a147d341e45ec72848324708d",
+    "url": "/react-hooks-redux-example/index.html"
+  },
+  {
+    "revision": "6452d447389f6e76518c",
+    "url": "/react-hooks-redux-example/static/css/main.5bb84f5d.chunk.css"
+  },
+  {
+    "revision": "c5be0308975248c4098b",
+    "url": "/react-hooks-redux-example/static/js/2.e492864d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-hooks-redux-example/static/js/2.e492864d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6452d447389f6e76518c",
+    "url": "/react-hooks-redux-example/static/js/main.8556c22a.chunk.js"
+  },
+  {
+    "revision": "48f437de23df89677853",
+    "url": "/react-hooks-redux-example/static/js/runtime-main.74e6fc5f.js"
+  }
+]);
